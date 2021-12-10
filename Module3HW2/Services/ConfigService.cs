b7256cad11e4199d9423b0ca390937a6e7576d3b
Dubrov Ruslan <dubrov.ruslan@gmail.com>
2021-12-10 03:05:22 +0200

    Done

---
+++ Module3HW2/Services/ConfigService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Module3HW2.Models;
+
+namespace Module3HW2.Services
+{
+    internal class ConfigService
+    {
+        private const string ConfigPath = "config.json";
+        public Config ReadConfig()
+        {
+            try
+            {
+                var configFile = File.ReadAllText(ConfigPath, System.Text.Encoding.UTF8);
+                return JsonConvert.DeserializeObject<Config>(configFile);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public void WriteConfig(Config config)
+        {
+            var configFile = JsonConvert.SerializeObject(config);
+            File.WriteAllText(ConfigPath, configFile);
+        }
+    }
+}
