1213b43cd5aab928e7485ae5ccc02ff7304b04c6
DubrovRuslan <dubrov.ruslan@gmail.com>
2023-06-20 23:56:07 +0300

    Done

---
+++ Starter.cs
@@ -0,0 +1,24 @@
+﻿using Module3HW2.Models;
+using Module3HW2.Services.Abstractions;
+
+namespace Module3HW2
+{
+    public class Starter
+    {
+        private readonly IContactService _contactProvider;
+        public Starter(IContactService contactProvider)
+        {
+            _contactProvider = contactProvider;
+        }
+
+        public void Run()
+        {
+            _contactProvider.AddContact("Руслан", "Дубров");
+            _contactProvider.AddContact("Петр", "Петренко", "436456", PhoneType.Mobile);
+            _contactProvider.AddContact("Петр", "Баранов", "32343235", PhoneType.Work);
+            _contactProvider.AddContact("Александр", "Зайченко", "2353443", PhoneType.Main);
+            _contactProvider.AddContact("Александр", "Зайченко", "+380973452312", PhoneType.Work);
+            _contactProvider.ShowAllContacts();
+        }
+    }
+}
